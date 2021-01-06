fcdf5d8f6f5e0accb638e5299003dfa735c9af60
xmxmqq <wjstnals97@gmail.com>
2021-01-06 10:46:06 +0900

    domain

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded   // Embedded, 또는 Embeddable(클래스에) 둘 중 하나의 어노테이션만 있어도 되긴 한다.
